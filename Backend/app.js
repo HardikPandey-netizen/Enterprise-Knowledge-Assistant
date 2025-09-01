2357d8cacb7946b393833521aa15aafdfe6c4c47
HardikPandey-netizen <hardikpandey120304@gmail.com>
2025-09-01 20:16:35 +0530

    Reduced Errors

--- Backend/app.js
+++ Backend/app.js
@@ -1,24 +1,27 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
-const userRouter = require('./routes/userRoutes');
-const cors = require('cors');
-const chatRouter = require('./routes/chatRoutes');
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+const userRouter = require("./routes/userRoutes");
+const cors = require("cors");
+const chatRouter = require("./routes/chatRoutes");
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/chats", chatRouter);
 
-app.use('/api/v1/users',userRouter);
-app.use('/api/v1/chats',chatRouter);
+app.get("/", () => {
+  res.send("I am Live");
+});
 
-app.all('*',(req,res,next)=>{
-    next(new AppError(`cannot find ${req.originalUrl} on this server.`,404))
-})
- 
+app.all("*", (req, res, next) => {
+  next(new AppError(`cannot find ${req.originalUrl} on this server.`, 404));
+});
+
 app.use(globalErrorHandler);
 
-module.exports = app; 
+module.exports = app;
